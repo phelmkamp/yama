772460b04541e7507edb1a79d7c0ad2e6a743985
Phil Helmkamp <phelmkamp+honiara@gmail.com>
2016-02-09 23:35:34 -0500

    beautification

--- src/main/java/com/ankara/honiara/api/controllers/MessageController.java
+++ src/main/java/com/ankara/honiara/api/controllers/MessageController.java
@@ -1,8 +1,6 @@
 package com.ankara.honiara.api.controllers;
 
 import java.security.Principal;
-import java.util.Collections;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -10,14 +8,9 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 import com.ankara.honiara.api.representations.Message;
-import com.ankara.honiara.persistence.entities.Conversation;
-import com.ankara.honiara.persistence.repositories.ConversationRepo;
 
 @Controller
 public class MessageController {
-
-	@Autowired
-	private ConversationRepo convoRepo;
 	
 	@Autowired
 	private SimpMessagingTemplate template;
@@ -26,16 +19,7 @@ public class MessageController {
 	public void process(Message msg, Principal principal, @Header String destination) {
 		msg.setSender(principal.getName());
 		
-		String convoId = destination.substring(destination.lastIndexOf('/'));
-		if (convoId.equals("/")) {
-			Conversation convo = new Conversation();
-			convo.setUsers(Collections.singleton(msg.getSender()));
-			convo = convoRepo.save(convo);
-			convoId = convo.getId();
-		} else {
-			convoId = convoId.substring(1);
-		}
-		
+		String convoId = destination.substring(destination.lastIndexOf('/') + 1);		
 		template.convertAndSend("/topic/convos/" + convoId, msg);
 	}
 }
